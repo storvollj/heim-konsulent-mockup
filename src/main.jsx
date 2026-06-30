804bd94dd4328e512940d21b4d927d0bef0bb57e
storvollj <storvollj@gmail.com>
2026-06-30 14:01:53 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HeimuKonsulentMockupLekmann from './heim-konsulent-mockup-lekmann'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HeimuKonsulentMockupLekmann />
+  </React.StrictMode>,
+)
